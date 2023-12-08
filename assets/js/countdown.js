6621cfb164bcf235e5e6401ba0773a47bb92166f
HegeMate <hegemate1209@gmail.com>
2023-12-08 18:48:14 +0100

    skeleton

---
+++ assets/js/countdown.js
@@ -0,0 +1,25 @@
+// Set the date for New Year's Day
+const newYearDate = new Date("January 1, 2024 00:00:00").getTime();
+
+// Update the countdown every second
+const countdown = setInterval(() => {
+  // Get the current time
+  const now = new Date().getTime();
+
+  // Calculate the distance between now and New Year's Day
+  const distance = newYearDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown
+  const countdownElement = document.getElementById("countdown");
+  countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  // If the countdown is over, display a message
+}, 1000);
